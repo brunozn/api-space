3f4fa88f43b6de5d608ad39d3e2747e45a95c50a
brunozn <brunojndias@gmail.com>
2021-05-20 00:55:52 -0300

    export xls with xlswriter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import os
 import xlsxwriter
-from datetime import datetime
+
+# from datetime import datetime
 TOKEN = os.getenv('TOKEN')
 URL = 'https://vortexz.jetbrains.space/api/http/projects/key:EVENLINEZ/planning/issues?sorting=CREATED&' \
       'descending=true&$fields=data(status(name,id),title,sprints(board(board(id,name)),description),' \
@@ -14,52 +15,50 @@ def expor_issue():
     for x in datas:
         for y in x["sprints"]:
             if y["board"]["board"]["name"] == '2021':
+                value = [(
+                    x["title"], x["description"],
+                    x["creationTime"]["iso"], x["status"]["name"]
+                )]
+
                 dates = [
-                    {"titulo": x["title"]},
-                    {"description": x["description"]},
-                    {"board": y["board"]["board"]["name"]},
-                    {"created": x["creationTime"]["iso"]},
-                    {"Status": x["status"]["name"]}
+                    {'Title': x["title"], 'Description': x["description"], 'Created':  x["creationTime"]["iso"], 'Status': x["status"]["name"]},
                 ]
-                return dates
+
+                book = xlsxwriter.Workbook('Issues.xlsx')
+                sheet = book.add_worksheet("Issues BOARD")
+                sheet.set_column('A:A', 60)
+                sheet.set_column('B:B', 40)
+                sheet.set_column('C:C', 30)
+                sheet.merge_range('A1:F1', "SPRINT", book.add_format({'bold': True, 'font_size': 14, 'align': 'center',
+                                                                      'valign': 'vcenter', 'font_name': 'Arial',
+                                                                      'color': '#171717'}))
+                bold = book.add_format({'bold': 1})
+                row_num = 1
+                columns = ['Title', 'Description', 'Created', 'Status']
+                for col_num in range(len(columns)):
+                    sheet.write(row_num, col_num, columns[col_num], bold)
+
+                example = [{'Title': 'MG1', 'Description': 'SP1', 'Created': 'RJ2', 'Status': 'CE4'},
+                           {'Title': 'MG2', 'Description': 'SP2', 'Created': 'RJ3', 'Status': 'CE3'},
+                           {'Title': 'MG3', 'Description': 'SP3', 'Created': 'RJ4', 'Status': 'CE2'},
+                           {'Title': 'MG4', 'Description': 'SP4', 'Created': 'RJ5', 'Status': 'CE1'},
+                           ]
+                for idy, data in enumerate(dates):
+                    print(idy, data)
+                    col = 0
+                    row = 2 + idy
+                    if data['Title'] is None:
+                        sheet.write_string(row, col, '', )
+                    else:
+                        sheet.write_string(row, col, data['Title'])
+                    if data['Description'] is None:
+                        sheet.write_string(row, col + 1, '', )
+                    else:
+                        sheet.write_string(row, col + 1, data['Description'])
+                    sheet.write_string(row, col + 2, data['Created'])
+                    sheet.write_string(row, col + 3, data['Status'])
+                book.close()
 
 
-def export_excel():
-    book = xlsxwriter.Workbook('Issues.xlsx')
-    sheet = book.add_worksheet("Issues BOARD")
-    sheet.merge_range('A1:F1', "SPRINT", book.add_format({'bold': True, 'font_size': 14, 'align': 'center',
-                                                          'valign': 'vcenter', 'font_name': 'Arial',
-                                                          'color': '#171717'}))
-    bold = book.add_format({'bold': 1})
-    date_format = book.add_format({'num_format': 'mmmm d yyyy'})
-    sheet.set_column(1, 1, 15)
-    sheet.write('A2', 'Title', bold)
-    sheet.write('B2', 'Description', bold)
-    sheet.write('C2', 'Created', bold)
-    sheet.write('D2', 'Board', bold)
-    sheet.write('E2', 'Status', bold)
-
-    example = (
-        ['Titulo 1', 'Description 1', '2013-01-13', 2021, 'Done'],
-        ['Titulo 2', 'Description 2', '2013-01-14', 2021, 'Open'],
-        ['Titulo 3', 'Description3', '2013-01-16', 2021, 'Done'],
-        ['Titulo 4', 'Description4 ', '2013-01-20', 2021, 'Testing'],
-    )
-
-    row = 2
-    col = 0
-    for item in example:
-        date = datetime.strptime(item[2], "%Y-%m-%d")
-
-        sheet.write_string(row, col, item[0])
-        sheet.write_string(row, col + 1, item[1])
-        sheet.write_datetime(row, col + 2, date, date_format)
-        sheet.write_number(row, col + 3, item[3])
-        sheet.write_string(row, col + 4, item[4])
-        row += 1
-
-    book.close()
-
-
-# expor_issue()
-export_excel()
+expor_issue()
+# export_excel(r)
